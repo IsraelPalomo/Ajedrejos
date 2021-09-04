7aba9f097686b7a1282401bb73e65b3b47304679
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-09-04 13:45:08 +0200

    Configuraciones fullpage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import "./styles.css";
 import "./styles.scss";
 import imgSRC from "./static/dama__blanca.png";
-const myFullpage = new fullpage("#Myfullpage", {});
+const myFullpage = new fullpage("#Myfullpage", {
+	autoScrolling: true,
+	fitToSection: true,
+	fitToSectionDelay: 1000,
+	easing: "easeInOutCubic",
+	scrollingSpeed: 1000,
+	easingcss3: "ease",
+	css3: true,
+	loopBottom: true,
+	navigation: true,
+	menu: "#menu",
+	anchors: ["inicio", "torneos", "contacto", "footer"],
+	navigationTooltips: ["Inicio", "Torneos", "Contacto", "Footer"],
+	showActiveTooltip: true,
+});
 
 let IMG = document.getElementById("img1");
 
