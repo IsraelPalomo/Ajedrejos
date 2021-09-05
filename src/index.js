8e8d76a307c080f1219716c7b0d4849afd47648f
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-09-05 09:48:52 +0200

    Terminada seccion 2 y repomsive añadido

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import "./styles.scss";
 AOS.init();
 import imgSRC from "./static/logotipo.png";
 import imgSRC2 from "./static/logotipo__pieza.png";
+import imgSRC3 from "./static/fuensalida.png";
+import imgSRC4 from "./static/villafranca.jpg";
+import imgSRC5 from "./static/madridejos.jpg";
 const myFullpage = new fullpage("#Myfullpage", {
 	autoScrolling: true,
 	fitToSection: true,
@@ -17,10 +20,18 @@ const myFullpage = new fullpage("#Myfullpage", {
 	anchors: ["inicio", "torneos", "contacto", "footer"],
 	navigationTooltips: ["Inicio", "Torneos", "Contacto", "Footer"],
 	showActiveTooltip: true,
+
+	verticalCentered: true,
 });
 
 let imgInicio = document.getElementById("img1Inicio");
 let imgLogo = document.getElementById("img__logo");
+let imgSlide1 = document.querySelector(".slide1");
+let imgSlide2 = document.querySelector(".slide2");
+let imgSlide3 = document.querySelector(".slide3");
 
 imgInicio.src = imgSRC;
 imgLogo.src = imgSRC2;
+imgSlide1.src = imgSRC3;
+imgSlide2.src = imgSRC4;
+imgSlide3.src = imgSRC5;
